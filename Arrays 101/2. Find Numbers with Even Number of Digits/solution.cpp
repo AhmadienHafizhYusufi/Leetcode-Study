73bb6445a9e5f1372e99ffca18f2d608c3525729
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-02-15 13:07:59 +0700

    Problems
    Finishing invert binary tree problem

---
+++ Arrays 101/2. Find Numbers with Even Number of Digits/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+        int findNumbers(vector<int>& nums) {
+            int count = 0;
+            for(int num : nums) {
+                if (digits % 2 == 0) {
+                    
+                }
+            }
+        }
+    };
