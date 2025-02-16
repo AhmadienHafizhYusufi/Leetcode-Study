004c972ee15b8a86757364c4507cff95387aa635
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-02-17 03:52:57 +0700

    Problems
    Finishing Squares of a Sorted Array problem.

--- Arrays 101/1. Max Consecutive Ones/README.md
+++ Arrays 101/1. Max Consecutive Ones/README.md
@@ -12,3 +12,5 @@ Given a binary array `nums`, return the *maximum number* of consecutive `1`'s in
 ## Constraints:
 - `1 <= nums.length <= 105`
 - `nums[i]` is either `0` or `1`.
+
+## Solution
--- Arrays 101/1. Max Consecutive Ones/solution.cpp
+++ Arrays 101/1. Max Consecutive Ones/solution.cpp
@@ -3,14 +3,25 @@ class Solution {
         int findMaxConsecutiveOnes(vector<int>& nums) {
             int max = 0;
             int count = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[i] == 1) {
+            // for (int i = 0; i < nums.size(); i++) {
+            //     if (nums[i] == 1) {
+            //         count++;
+            //         if (count > max) {
+            //             max = count;
+            //         }
+            //     } else {
+            //         count = 0;
+            //     }
+            // }
+
+            for (int num : nums) {
+                if (num == 1) {
                     count++;
                     if (count > max) {
                         max = count;
+                    } else {
+                        count = 0;
                     }
-                } else {
-                    count = 0;
                 }
             }
             return max;
