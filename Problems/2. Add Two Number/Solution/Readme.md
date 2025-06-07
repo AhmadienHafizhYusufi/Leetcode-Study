14d5f93b7e8f9d662d270470f5758c93b1c76b5f
AHMADIEN HAFIZH YUSUFI <ahmadienhafizhyusufi@gmail.com>
2025-06-07 08:36:01 +0700

    4. Median of Two Sorted Arrays (Uncomplete)

---
+++ Problems/2. Add Two Number/Solution/Readme.md
@@ -0,0 +1,17 @@
+# Add Two Numbers - Solution
+
+## Prerequisite
+
+Before we do this problem in C++.
+
+- pointer
+- struct
+
+## Approach 1: Elementary Math
+
+Just like how to sum two numbers on a piece of paper but the digits are stored
+in reverse order.
+
+### Pesudocode
+
+- Initialize current node to dummy head of the returning list
---
+++ Problems/38 - Count and Say/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string currentString = "1";
+        for (int i = 2; i <= n; i++) {
+            string nextString;
+            for (int j = 0, k =0; j < currentString.size(); j = k) {
+                
+            }
+        }
+    }
+};
